4eee76ccddcb02de5fb684511bad7971fb511701
Felipe Osorio <faosorios@gmail.com>
2020-05-15 20:50:15 -0400

    Add files via upload

--- segmented/segmentedChile.R
+++ segmented/segmentedChile.R
@@ -10,6 +10,9 @@ chile <- read.csv(url("https://raw.githubusercontent.com/MinCiencia/Datos-COVID1
 dbnames <- c("Fecha", "Arica.y.Parinacota", "Tarapaca", "Antofagasta", "Atacama", "Coquimbo", "Valparaiso", "Metropolitana",
   "O.Higgins", "Maule", "Nuble", "Biobio", "Araucania", "Los.Rios", "Los.Lagos", "Aysen", "Magallanes", "Total")
 names(chile) <- dbnames
+npsi <- 1:15 # number of 'periods'
+hundred <- ">=100" # more than 100 cases
+zero <- ">=0" # more than 100 cases
 
 ## loading required package
 library(segmented)
@@ -21,71 +24,71 @@ now <- proc.time()
 ## fitting models for each 'region'
 y <- chile$Total
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oChile <- segmented19(y, npsi = 1:10, rule = ">=0")
+oChile <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$Arica.y.Parinacota
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oArica <- segmented19(y, npsi = 1:10, rule = ">=0")
+oArica <- segmented19(y, npsi = npsi, rule = zero) # >= 0
 
 y <- chile$Tarapaca
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oTarapaca <- segmented19(y, npsi = 1:10, rule = ">=0")
+oTarapaca <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$Antofagasta
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oAntofagasta <- segmented19(y, npsi = 1:10, rule = ">=0")
+oAntofagasta <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$Atacama
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oAtacama <- segmented19(y, npsi = 1:10, rule = ">=0")
+oAtacama <- segmented19(y, npsi = npsi, rule = zero) # >= 0
 
 y <- chile$Coquimbo
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oCoquimbo <- segmented19(y, npsi = 1:10, rule = ">=0")
+oCoquimbo <- segmented19(y, npsi = npsi, rule = zero) # >= 0
 
 y <- chile$Valparaiso
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oValpo <- segmented19(y, npsi = 1:10, rule = ">=0")
+oValpo <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$Metropolitana
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oStgo <- segmented19(y, npsi = 1:10, rule = ">=0")
+oStgo <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$O.Higgins
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oOHiggins <- segmented19(y, npsi = 1:10, rule = ">=0")
+oOHiggins <- segmented19(y, npsi = npsi, rule = zero) # >= 0
 
 y <- chile$Maule
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oMaule <- segmented19(y, npsi = 1:10, rule = ">=0")
+oMaule <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$Nuble
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oNuble <- segmented19(y, npsi = 1:10, rule = ">=0")
+oNuble <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$Biobio
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oBiobio <- segmented19(y, npsi = 1:10, rule = ">=0")
+oBiobio <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$Araucania
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oAraucania <- segmented19(y, npsi = 1:10, rule = ">=0")
+oAraucania <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$Los.Rios
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oLosRios <- segmented19(y, npsi = 1:10, rule = ">=0")
+oLosRios <- segmented19(y, npsi = npsi, rule = zero) # >= 0
 
 y <- chile$Los.Lagos
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oLosLagos <- segmented19(y, npsi = 1:10, rule = ">=0")
+oLosLagos <- segmented19(y, npsi = npsi, rule = hundred)
 
 y <- chile$Aysen
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oAysen <- segmented19(y, npsi = 1:10, rule = ">=0")
+oAysen <- segmented19(y, npsi = npsi, rule = zero) # >= 0
 
 y <- chile$Magallanes
 attr(y, "first.date") <- as.character(chile$Fecha[1])
-oMagallanes <- segmented19(y, npsi = 1:10, rule = ">=0")
+oMagallanes <- segmented19(y, npsi = npsi, rule = hundred)
 
 speed <- proc.time() - now
 rm(now, y) # cleaning
